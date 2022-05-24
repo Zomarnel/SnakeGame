8098b0ea8d861f0168846f56f59ef815c5ac61de
Bouazza <Bouazza@DESKTOP-BE1S3RB>
2022-05-24 15:43:28 +0200

    Update

--- WPFUI/UI/Windows/PlayAgainMessage.xaml.cs
+++ WPFUI/UI/Windows/PlayAgainMessage.xaml.cs
@@ -1,10 +1,24 @@
-﻿using System.Windows;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
 
 namespace WPFUI.UI.Windows
 {
     public partial class PlayAgainMessage : Window
     {
+        [DllImport("user32.dll")]
+        static extern IntPtr GetSystemMenu(IntPtr hWnd, bool bRevert);
+
+        [DllImport("user32.dll")]
+        static extern bool EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+
+        const uint MF_BYCOMMAND = 0x00000000;
+        const uint MF_GRAYED = 0x00000001;
+
+        const uint SC_CLOSE = 0xF060;*/ 
+
         public PlayAgainMessage()
         {
             InitializeComponent();
@@ -13,15 +27,26 @@ namespace WPFUI.UI.Windows
         {
             Close();
         }
-
         private void OnMouseEnter(object sender, MouseEventArgs e)
         {
             Cursor = Cursors.Hand;
         }
-
         private void OnMouseLeave(object sender, MouseEventArgs e)
         {
             Cursor = Cursors.Arrow;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            base.OnSourceInitialized(e);
+
+            //This will disable the close button
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            IntPtr hMenu = GetSystemMenu(hwnd, false);
+            if (hMenu != IntPtr.Zero)
+            {
+                EnableMenuItem(hMenu, SC_CLOSE, MF_BYCOMMAND | MF_GRAYED);
+            }
         }
     }
 }
